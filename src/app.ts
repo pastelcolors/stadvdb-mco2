a0d98bdcc09ae0ff77e03bdb38f7ab031ea64e0d
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-04-07 16:01:55 +0800

    feat: initialize movie repository

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import { MovieRepository } from "./repository/movies.repository";
+import { centralNode } from "./config/db";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -6,7 +8,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/", async (req, res) => {
-  return res.send("Hello world")
+  const movies = MovieRepository(centralNode);
+  const { rows } = await movies.findAll();
+
+  return res.send(rows);
 });
 
 const server = app.listen(PORT, () =>
