502946e6aa92ccb0b6db75c0b1a372c99f5cbd69
TyroneStaMaria <tyrone.stamaria35@gmail.com>
2023-04-16 01:05:47 +0800

    feat: modify get movies to still get data when central node is down

--- src/utils/queries.ts
+++ src/utils/queries.ts
@@ -36,9 +36,9 @@ export const createMovie = async (movie: Movie) => {
 
   try {
     // Start phase
-    await mainConnection.query("XA START ?;", phase); 
+    await mainConnection.query("XA START ?;", phase);
     await shardConnection.query("XA START ?;", phase);
-    
+
     // Insert the movie in both the central node and the shard
     await mainConnection.query("INSERT INTO movies SET ?", { id, ...movie });
     await shardConnection.query("INSERT INTO movies SET ?", { id, ...movie });
@@ -58,7 +58,7 @@ export const createMovie = async (movie: Movie) => {
     await mainConnection.commit();
     await shardConnection.commit();
 
-    return {id, ...movie};
+    return { id, ...movie };
   } catch (error) {
     await mainConnection.rollback();
     await shardConnection.rollback();
@@ -69,31 +69,52 @@ export const createMovie = async (movie: Movie) => {
   }
 };
 
-
 export const getMovie = async (id: string) => {
-  const centralConnection = await centralNodePool.getConnection();
-  const before1980Connection = await before1980NodePool.getConnection();
-  const after1980Connection = await after1980NodePool.getConnection();
+  let centralConnection, before1980Connection, after1980Connection;
 
   try {
-    const [centralResult] = await centralConnection.query(
-      "SELECT * FROM movies WHERE id = ?",
-      [id]
-    );
-    const [node2Result] = await before1980Connection.query(
-      "SELECT * FROM movies WHERE id = ?",
-      [id]
-    );
-    const [node3Result] = await after1980Connection.query(
-      "SELECT * FROM movies WHERE id = ?",
-      [id]
-    );
+    centralConnection = await centralNodePool.getConnection();
+  } catch (e) {
+    console.error("Failed to connect to centralNodePool:", e);
+  }
 
-    return centralResult || node2Result || node3Result || null;
+  try {
+    before1980Connection = await before1980NodePool.getConnection();
+  } catch (e) {
+    console.error("Failed to connect to before1980NodePool:", e);
+  }
+
+  try {
+    after1980Connection = await after1980NodePool.getConnection();
+  } catch (e) {
+    console.error("Failed to connect to after1980NodePool:", e);
+  }
+
+  try {
+    const centralResult = centralConnection
+      ? await centralConnection.query("SELECT * FROM movies WHERE id = ?", [id])
+      : [null];
+    const node2Result = before1980Connection
+      ? await before1980Connection.query("SELECT * FROM movies WHERE id = ?", [
+          id,
+        ])
+      : [null];
+    const node3Result = after1980Connection
+      ? await after1980Connection.query("SELECT * FROM movies WHERE id = ?", [
+          id,
+        ])
+      : [null];
+
+    const results = [centralResult, node2Result, node3Result];
+    const [movie] = results.find(
+      (result) => result[0] && (result[0] as []).length > 0
+    ) || [null];
+
+    return movie;
   } finally {
-    centralConnection.release();
-    before1980Connection.release();
-    after1980Connection.release();
+    if (centralConnection) centralConnection.release();
+    if (before1980Connection) before1980Connection.release();
+    if (after1980Connection) after1980Connection.release();
   }
 };
 
@@ -103,7 +124,6 @@ export const updateMovie = async (movie: Movie) => {
   const shardConnection = await shardPool.getConnection();
 
   try {
-
     // Start phase
     await mainConnection.query("XA START ?;", movie.id);
     await shardConnection.query("XA START ?;", movie.id);
@@ -121,7 +141,6 @@ export const updateMovie = async (movie: Movie) => {
     // End phase
     await mainConnection.query("XA END ?;", movie.id);
     await shardConnection.query("XA END ?;", movie.id);
-
 
     // Prepare phase
     await mainConnection.query("XA PREPARE ?;", movie.id);
@@ -155,7 +174,7 @@ export const deleteMovie = async (id: string) => {
     await centralConnection.query("XA START ?;", id);
     await before1980Connection.query("XA START ?;", id);
     await after1980Connection.query("XA START ?;", id);
-    
+
     // Delete the movie in all the nodes
     await centralConnection.query("DELETE FROM movies WHERE id = ?", [id]);
     await before1980Connection.query("DELETE FROM movies WHERE id = ?", [id]);
@@ -165,7 +184,7 @@ export const deleteMovie = async (id: string) => {
     await centralConnection.query("XA END ?;", id);
     await before1980Connection.query("XA END ?;", id);
     await after1980Connection.query("XA END ?;", id);
-    
+
     // Prepare phase
     await centralConnection.query("XA PREPARE ?;", id);
     await before1980Connection.query("XA PREPARE ?;", id);
